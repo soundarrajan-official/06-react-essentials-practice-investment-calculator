dfef027b30027de74dfec6ac842472b771956030
soundarrajan-official <soundarrajan.official@gmail.com>
2024-05-01 19:53:17 +0530

    Add UserInput States and handle event changes using two way binding

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,24 +1,67 @@
+import {useState} from "react";
+
 const UserInput = () => {
+    const [initialInvestment, setInitialInvestment] = useState(10000)
+    const [annualInvestment, setAnnualInvestment] = useState(1200)
+    const [expectedReturn, setExpectedReturn] = useState(6)
+    const [duration, setDuration] = useState(10)
+
+    const handleInitialInvestmentChange = (event) => {
+        setInitialInvestment(event.target.value)
+    }
+
+    const handleAnnualInvestmentChange = (event) => {
+        setAnnualInvestment(event.target.value)
+    }
+
+    const handleExpectedReturnChange = (event) => {
+        setExpectedReturn(event.target.value)
+    }
+
+    const handleDurationChange = (event) => {
+        setDuration(event.target.value)
+    }
+
     return (
         <section id="user-input">
             <div className="input-group">
                 <p>
                     <label>Initial Investment</label>
-                    <input type="number" required/>
+                    <input
+                        type="number"
+                        required
+                        onChange={handleInitialInvestmentChange}
+                        value={initialInvestment}
+                    />
                 </p>
                 <p>
                     <label>Annual Investment</label>
-                    <input type="number" required/>
+                    <input
+                        type="number"
+                        required
+                        onChange={handleAnnualInvestmentChange}
+                        value={annualInvestment}
+                    />
                 </p>
             </div>
             <div className="input-group">
                 <p>
                     <label>Expected Return</label>
-                    <input type="number" required/>
+                    <input
+                        type="number"
+                        required
+                        onChange={handleExpectedReturnChange}
+                        value={expectedReturn}
+                    />
                 </p>
                 <p>
                     <label>Duration</label>
-                    <input type="number" required/>
+                    <input
+                        type="number"
+                        required
+                        onChange={handleDurationChange}
+                        value={duration}
+                    />
                 </p>
             </div>
         </section>
