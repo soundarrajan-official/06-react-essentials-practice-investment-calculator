2e71091c1d46e64616f9812e8a6b802219f3fcec
soundarrajan-official <soundarrajan.official@gmail.com>
2024-05-01 20:16:16 +0530

    Use one investmentData State and handler instead of multiple State and handler

--- src/components/UserInput/UserInput.jsx
+++ src/components/UserInput/UserInput.jsx
@@ -1,25 +1,49 @@
 import {useState} from "react";
 
+const initialInvestmentData = {
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+
+}
+
 const UserInput = () => {
-    const [initialInvestment, setInitialInvestment] = useState(10000)
-    const [annualInvestment, setAnnualInvestment] = useState(1200)
-    const [expectedReturn, setExpectedReturn] = useState(6)
-    const [duration, setDuration] = useState(10)
+    /**
+     *     const [initialInvestment, setInitialInvestment] = useState(10000)
+     *     const [annualInvestment, setAnnualInvestment] = useState(1200)
+     *     const [expectedReturn, setExpectedReturn] = useState(6)
+     *     const [duration, setDuration] = useState(10)
+     */
 
-    const handleInitialInvestmentChange = (event) => {
-        setInitialInvestment(event.target.value)
-    }
+    const [investmentData, setInvestmentData] = useState(initialInvestmentData)
 
-    const handleAnnualInvestmentChange = (event) => {
-        setAnnualInvestment(event.target.value)
-    }
+    /**
+     *  const handleInitialInvestmentChange = (event) => {
+     *         setInitialInvestment(event.target.value)
+     *     }
+     *
+     *     const handleAnnualInvestmentChange = (event) => {
+     *         setAnnualInvestment(event.target.value)
+     *     }
+     *
+     *     const handleExpectedReturnChange = (event) => {
+     *         setExpectedReturn(event.target.value)
+     *     }
+     *
+     *     const handleDurationChange = (event) => {
+     *         setDuration(event.target.value)
+     *     }
+     */
 
-    const handleExpectedReturnChange = (event) => {
-        setExpectedReturn(event.target.value)
-    }
-
-    const handleDurationChange = (event) => {
-        setDuration(event.target.value)
+    const handleInvestmentDataChange = (event) => {
+        const {name, value} = event.target
+        setInvestmentData((prevInvestmentData) => {
+            return {
+                ...prevInvestmentData,
+                [name]: value
+            }
+        })
     }
 
     return (
@@ -30,8 +54,11 @@ const UserInput = () => {
                     <input
                         type="number"
                         required
-                        onChange={handleInitialInvestmentChange}
-                        value={initialInvestment}
+                        // onChange={handleInitialInvestmentChange}
+                        // value={initialInvestment}
+                        name="initialInvestment"
+                        onChange={handleInvestmentDataChange}
+                        value={investmentData.initialInvestment}
                     />
                 </p>
                 <p>
@@ -39,8 +66,11 @@ const UserInput = () => {
                     <input
                         type="number"
                         required
-                        onChange={handleAnnualInvestmentChange}
-                        value={annualInvestment}
+                        // onChange={handleAnnualInvestmentChange}
+                        // value={annualInvestment}
+                        name="annualInvestment"
+                        onChange={handleInvestmentDataChange}
+                        value={investmentData.annualInvestment}
                     />
                 </p>
             </div>
@@ -50,8 +80,11 @@ const UserInput = () => {
                     <input
                         type="number"
                         required
-                        onChange={handleExpectedReturnChange}
-                        value={expectedReturn}
+                        // onChange={handleExpectedReturnChange}
+                        // value={expectedReturn}
+                        name="expectedReturn"
+                        onChange={handleInvestmentDataChange}
+                        value={investmentData.expectedReturn}
                     />
                 </p>
                 <p>
@@ -59,8 +92,11 @@ const UserInput = () => {
                     <input
                         type="number"
                         required
-                        onChange={handleDurationChange}
-                        value={duration}
+                        // onChange={handleDurationChange}
+                        // value={duration}
+                        name="duration"
+                        onChange={handleInvestmentDataChange}
+                        value={investmentData.duration}
                     />
                 </p>
             </div>
